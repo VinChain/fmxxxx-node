a511def41abf4fabf80fe7d3de49af607334d07c
Alexander Sergeychik <alexander@brainq.com>
2019-01-17 10:07:07 +0300

    V2 draft fix 3

--- src/fmxxxx/server.ts
+++ src/fmxxxx/server.ts
@@ -57,9 +57,15 @@ export class FmxxxxServer extends EventEmitter implements ServerEvents {
 
 		this.fmxxxx.on('connection', (device) => this.emit('connection', device));
 
-		this.fmxxxx.on('record', (device, record) => FmxxxxServer.mapCodec8(device, record));
-		this.fmxxxx.on('command', (device, command) => FmxxxxServer.mapCodec12(device, command));
-		this.fmxxxx.on('info', (device, telemetry, timestamp) => FmxxxxServer.mapInfo(device, telemetry, timestamp));
+		this.fmxxxx.on('record', (device, record) => {
+			this.emit('data', FmxxxxServer.mapCodec8(device, record), device);
+		});
+		this.fmxxxx.on('command', (device, command) => {
+			this.emit('data', FmxxxxServer.mapCodec12(device, command), device);
+		});
+		this.fmxxxx.on('info', (device, telemetry, timestamp) => {
+			this.emit('data',	FmxxxxServer.mapInfo(device, telemetry, timestamp), device);
+		});
 	}
 
 	public listen(...args: any[]) {
